247f11b250fa236ecf2d43aebd3719304b070138
Mark Chambers <marksy@Rio.home>
2017-01-12 23:33:09 +0000

    added routes and firebase read/write

--- src/js/utilities/beeroclock.js
+++ src/js/utilities/beeroclock.js
@@ -1,36 +1,45 @@
 (function() {
-'use strict';
-var moment = require('moment');
-var runTimestamp = Math.round(Date.now());
-var intervalCount = 1000;
+  'use strict';
+  var moment = require('moment');
+  var runTimestamp = Math.round(Date.now());
+  var intervalCount = 60000; //every minute
 
-function beerOclock(beerDay) {
+  function beerOclock(beerDay) {
 
-  var dayOfWeek = beerDay || 5;//friday
-  var date = new Date(runTimestamp);
-  var msg;
-  var diff = date.getDay() - dayOfWeek;
-  if (diff > 0) {
-      date.setDate(date.getDate() + 6);
+    var dayOfWeek = beerDay || 5;//friday
+    var date = new Date(runTimestamp);
+    var msg;
+    var diff = date.getDay() - dayOfWeek;
+    if (diff > 0) {
+        date.setDate(date.getDate() + 6);
+    }
+    else if (diff < 0) {
+        date.setDate(date.getDate() + ((-1) * diff));
+    }
+    date.setHours(17);
+    date.setMinutes(0);
+    date.setSeconds(0);
+
+    //on the day speed up the intervalCount
+    if(diff === 0) {
+      intervalCount = 1000;
+    }
+
+    msg = "Beer o'clock " + moment(date).fromNow();
+    console.log(msg);
+
+    setTimeout(function() {
+      var beer = document.getElementById("beer");
+      beer.innerHTML = msg;
+      // console.log(beer);
+    }, 100);
+
   }
-  else if (diff < 0) {
-      date.setDate(date.getDate() + ((-1) * diff));
-  }
-  date.setHours(17);
-  date.setMinutes(0);
-  date.setSeconds(0);
 
-  msg = "Beer o'clock " + moment(date).fromNow();
-  console.log(msg);
-
-  var beer = document.getElementById("beer");
-  beer.innerHTML = msg;
-}
-
-  beerOclock();
-
-  setInterval(function() {
     beerOclock();
-  }, intervalCount);
+
+    setInterval(function() {
+      beerOclock();
+    }, intervalCount);
 
 })();
